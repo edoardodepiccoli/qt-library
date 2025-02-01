1c05f99cc1ea249db14d61e45885be853e098700
Edoardo De Piccoli <edoardodepiccoli2@gmail.com>
2025-02-02 00:12:38 +0100

    edit main

--- main.cpp
+++ main.cpp
@@ -1,22 +1,14 @@
 #include "book.h"
 #include "library.h"
 #include "movie.h"
-#include "debugvisitor.h"
 #include "mainwindow.h"
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
-    DebugVisitor *debugVisitor = new DebugVisitor();
     Book *book = new Book("harry potter", "jk rowling");
     Movie *movie = new Movie("interstellar", 2015);
-
-    Item *bookItem = book;
-    Item *movieItem = movie;
-
-    bookItem->accept(debugVisitor);
-    movieItem->accept(debugVisitor);
 
     Library *library = new Library();
     library->addItem(book);
