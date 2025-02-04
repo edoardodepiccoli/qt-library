ee6b4f9ee9d2fd7654683a937e1257eccc457dbb
Edoardo De Piccoli <edoardodepiccoli2@gmail.com>
2025-02-04 12:17:03 +0100

    remove unused view components and associated assets to streamline the project

--- qt-library.pro
+++ qt-library.pro
@@ -12,10 +12,6 @@ SOURCES += main.cpp \
     src/visitors/visitor.cpp \
     src/visitors/widgetvisitor.cpp \
     src/views/mainwindow.cpp \
-    src/views/components/sidebar.cpp \
-    src/views/searchview.cpp \
-    src/views/formview.cpp \
-    src/views/components/searchsection.cpp
 
 HEADERS += src/models/item.h \
     src/models/book.h \
@@ -24,10 +20,6 @@ HEADERS += src/models/item.h \
     src/visitors/visitor.cpp \
     src/visitors/widgetvisitor.cpp \
     src/views/mainwindow.h \
-    src/views/components/sidebar.h \
-    src/views/searchview.h \
-    src/views/formview.h \
-    src/views/components/searchsection.h
 
 RESOURCES += src/storage/items.json \
     src/views/assets/icons.qrc
--- src/views/mainwindow.cpp
+++ src/views/mainwindow.cpp
@@ -15,37 +15,7 @@ MainWindow::MainWindow(Library *library, QWidget *parent)
     layout->setContentsMargins(0, 0, 0, 0);
     layout->setSpacing(0);
 
-    // Create and add sidebar
-    sidebar = new Sidebar(this);
-    sidebar->setFixedWidth(66);
-    layout->addWidget(sidebar);
-
-    // Create views
-    searchView = new SearchView(this);
-    formView = new FormView(this);
-
-    // Initially show search view and hide form view
-    layout->addWidget(searchView);
-    layout->addWidget(formView);
-    formView->hide();
-
-    // Connect sidebar signals
-    connect(sidebar, &Sidebar::searchButtonClicked, this, &MainWindow::handleSearchView);
-    connect(sidebar, &Sidebar::addButtonClicked, this, &MainWindow::handleFormView);
-
     setCentralWidget(centralWidget); // Set the central widget of the main window
-}
-
-void MainWindow::handleSearchView()
-{
-    searchView->show();
-    formView->hide();
-}
-
-void MainWindow::handleFormView()
-{
-    searchView->hide();
-    formView->show();
 }
 
 MainWindow::~MainWindow()
