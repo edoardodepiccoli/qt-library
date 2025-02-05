ad145117678c8be340f6b2b6e3905f75c2cb637f
Edoardo De Piccoli <edoardodepiccoli2@gmail.com>
2025-02-05 01:19:20 +0100

    create newItemButton and wrap it in container with searchBox

--- src/views/mainwindow.cpp
+++ src/views/mainwindow.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include <QHBoxLayout>
 #include <QLineEdit>
+#include <QPushButton>
 
 MainWindow::MainWindow(Library *library, QWidget *parent)
     : QMainWindow(parent), library(library)
@@ -24,17 +25,35 @@ MainWindow::MainWindow(Library *library, QWidget *parent)
     layout->setContentsMargins(0, 0, 0, 0);
     layout->setSpacing(0);
 
+    // Create actions layout
+    QHBoxLayout *actionsLayout = new QHBoxLayout(centralWidget);
+    layout->setContentsMargins(0, 0, 0, 0);
+    layout->setSpacing(0);
+
+    // Create actions container
+    QWidget *actionsContainer = new QWidget(centralWidget);
+    actionsContainer->setLayout(actionsLayout);
+
     // Create search bar
     searchBox = new QLineEdit(centralWidget);
     searchBox->setPlaceholderText("Search...");
-    searchBox->setMaximumHeight(50);
-    searchBox->setStyleSheet("QLineEdit { padding: 10px; background-color: #2D2D2D; color: #FFFFFF; border: 1px solid #3D3D3D; }");
+    searchBox->setMaximumHeight(30);
+    searchBox->setStyleSheet("QLineEdit { padding: 5px; background-color: #2D2D2D; color: #FFFFFF; border: 1px solid #3D3D3D; }");
     connect(searchBox, &QLineEdit::textChanged, this, &MainWindow::handleSearch);
 
     // Create and add items container
     itemsContainer = new ItemsContainer(library);
 
-    layout->addWidget(searchBox);
+    // Create new item page button
+    QPushButton *newItemButton = new QPushButton(centralWidget);
+    newItemButton->setText("NEW ITEM");
+    newItemButton->setStyleSheet("QPushButton { padding: 10px; margin-bottom: 10px; margin-top: 10px; background-color: #3D3D3D; color: #D9D9D9; border: none; text-align: left } QPushButton::hover { background-color: #D9D9D9; color: #3D3D3D }");
+    newItemButton->setCursor(Qt::PointingHandCursor);
+    newItemButton->setFixedHeight(50);
+
+    actionsLayout->addWidget(searchBox, 0);
+    actionsLayout->addWidget(newItemButton, 0);
+    layout->addWidget(actionsContainer);
     layout->addWidget(itemsContainer);
     setCentralWidget(centralWidget);
 
